5e3e447760d14c4af4bb3fe638722bac9ee14aa4
dimasm93 <dimas_m@icloud.com>
2021-03-12 02:10:38 +0700

    create user

--- 01-basic-query/12-user-managements/create-user.sql
+++ 01-basic-query/12-user-managements/create-user.sql
@@ -1,1 +1,13 @@
 select sys_context('userenv', 'con_name') from dual;
+
+select * from USER_TABLESPACES;
+
+create user toko_online
+    identified by toko_online
+    default tablespace users
+    quota 10 M on users
+    account unlock;
+
+select * from dba_users where USERNAME = 'TOKO_ONLINE';
+
+grant create session to toko_online;
